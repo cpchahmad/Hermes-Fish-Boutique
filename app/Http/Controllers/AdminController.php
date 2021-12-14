fea5e9d0d8ecf8756dcafd100796ad240bcf3550
your_usernameqadeer-1 <82930325+qadeer-1@users.noreply.github.com>
2021-12-14 20:15:27 +0500

    commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -231,10 +231,10 @@ class AdminController extends Controller
     }
     public function checkout_data(Request $request){
 
-        $check_items = json_decode(json_encode($request->checkout_items));
+        $check_items = /*json_decode(json_encode(*/$request->checkout_items/*))*/;
         $o = new CheckoutOrder();
 //        $o->note = $check_items->note;
-        dd($check_items);
+        dd($check_items['currency']);
         if($check_items !=null){
 
 
